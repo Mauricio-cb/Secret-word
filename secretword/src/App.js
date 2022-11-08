917aee957406ff271b1c132150ee75238cd5c700
Mauricio-cb <mbispo1202@gmail.com>
2022-11-08 08:51:51 -0300

    Criando  componentes

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -1,10 +1,41 @@
+//Css
 import "./App.css";
+
+//React
+
+import { useState, useCallback, useEffect } from "react";
+
+//Data
+
+//Componentes
 import { StartScreen } from "./components/StartScreen";
+import GameOver from "./components/GameOver";
+import Game from "./components/Game";
+
+const stage = [
+  {
+    id: 1,
+    name: "start",
+  },
+  {
+    id: 2,
+    name: "game",
+  },
+  {
+    id: 3,
+    name: "end",
+  },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState([0].name);
+  const [words] = useState([1, 2, 3]);
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
     </div>
   );
 }
